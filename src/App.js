f7fc7363ece60e78530e5dba990c40559255e97d
unknown <mirazovro@gmail.com>
2022-10-10 19:03:36 +0600

    finally-rest-countries-done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Countries from './Components/Countries/Countries';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='App'>
+    <Countries></Countries>
+   </div>
   );
 }
 
+// function LoadCountries() {
+//   const [countries, setCountries] = useState([])
+
+//   useEffect(() => {
+//     fetch('https://restcountries.com/v3.1/all')
+//       .then(res => res.json())
+//       .then(data => setCountries(data))
+  
+//   }, [])
+//   return (
+//     <div>
+//       <h1>Visting every country of the world</h1>
+//       <h3>Available countries: {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common} population={country.population} region={country.region}> </Country>
+//       )}
+//     </div>
+//   )
+// }
+
+// function Country(props) {
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <h2>Population:{props.population}</h2>
+//       <h3>Region:{props.region}</h3>
+//     </div>
+//   )
+// }
 export default App;
